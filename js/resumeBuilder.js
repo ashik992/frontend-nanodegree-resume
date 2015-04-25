5cb29872788d4348943ab97265061dda2677e8ff
Ashik Ahmed <ashik.992@gmail.com>
2015-04-26 04:36:25 +0600

    Clean

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,8 @@
-$("#main").append("Ashik Ahmed");
+var formatedName = HTMLheaderName.replace("%data%", "Ashik Ahmed");
 
+var skills = ["Awesomeness", "Programming", "Photography", "Skateboarding"];
+var role ="programmer";
+var formatedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formatedRole);
+$("#header").prepend(formatedName);
